a70270ca66240fddf45ed1688c7a4f346f4ba30d
nobalmohan <nobal20@gmail.com>
2015-04-30 21:06:53 +0530

    HTML5 geolocation api

---
+++ production/scripts/main-80516176.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("nearbyrestaurants",["ui.router"]).config(["$stateProvider","$rootScopeProvider","$urlRouterProvider",function(e,t,r){t.digestTtl(100),r.otherwise("/"),e.state("main",{url:"/",templateUrl:"views/main.html",resolve:{}}).state("hospitals",{url:"/hospitals",templateUrl:"views/hospitals.html",resolve:{}})}]);
